063934cf76ba0c9013129305d08a023de3f191b6
Alessandro Chinchilla <ale030401@gmail.com>
2023-12-11 12:04:13 -0600

    Addes functionality to tabs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,52 @@
 import initLoad from "./modules/home";
+import renderMenuTabenuTab from "./modules/menu";
+import renderAboutTab from "./modules/about";
 import './styles/main.css';
 
 initLoad();
+ 
+function initializeTabs() {
+    const tabs = document.querySelectorAll('.tab');
+    const homeTab = document.querySelector('.homeTab');
+    const menuTab = document.querySelector('.menuTab');
+    const aboutTab = document.querySelector('.aboutTab');
+    const mainContent = document.querySelector('main');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            console.log(`Clicked on ${tab.textContent}`);
+           
+            if (tab !== homeTab) {
+                mainContent.innerHTML = ''; 
+            };
+
+            // Add logic to load content for each tab 
+        });
+    });
+
+    homeTab.addEventListener('click', () => {
+        const wholeContent = document.querySelector('#content');
+
+        if (mainContent.innerHTML === "") {
+            wholeContent.innerHTML = "";
+            initLoad(); 
+
+            // Reinitialize the tabs and their event listeners
+            initializeTabs();
+        };
+    });
+};
+
+// Initialize tabs and their event listeners
+initializeTabs();
+
+/*
+menuTab.addEventListener('click',()=>{
+    mainContent.innerHTML = ""
+    console.log('test')
+})
+
+aboutTab.addEventListener('click',()=>{
+    mainContent.innerHTML = ""
+})
+*/
