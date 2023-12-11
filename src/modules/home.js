5b227426842afc44925abe7a2b92f9b836bfe58f
Alessandro Chinchilla <ale030401@gmail.com>
2023-12-11 12:03:35 -0600

    Changed tab names

--- src/modules/home.js
+++ src/modules/home.js
@@ -6,46 +6,56 @@ export default function initLoad(){
     const description = document.createElement('p');
     const nav = document.createElement('nav');
     const list = document.createElement('ul');
-    const listItem0 = document.createElement('li');
-    const listItem1 = document.createElement('li');
-    const listItem2 = document.createElement('li');
-    const listItem3 = document.createElement('li');
+    const homeTab = document.createElement('li');
+    const menuTab = document.createElement('li');
+    const aboutTab = document.createElement('li');
+    const locationTab = document.createElement('li');
     const main = document.createElement('main');
     const article = document.createElement('article');
     const mainText = document.createElement('h1');
     const subText1 = document.createElement('p');
     const subText2 = document.createElement('p');
+    const buttonBox = document.createElement('div')
+    const menuButton = document.createElement('button');
+    const infoButton = document.createElement('button');
 
     const footer = document.createElement('footer');
 
-    listItem0.classList.add('tab');
-    listItem1.classList.add('tab');
-    listItem2.classList.add('tab');
-    listItem3.classList.add('tab');
+    homeTab.classList.add('tab', 'homeTab');
+    menuTab.classList.add('tab', 'menuTab');
+    aboutTab.classList.add('tab', 'aboutTab');
+    locationTab.classList.add('tab', 'locationTab');
     mainText.classList.add('mainText');
     subText1.classList.add('subText1');
     subText2.classList.add('subText2');
+    buttonBox.classList.add('buttonBox');
+    menuButton.classList.add('articleButton');
+    infoButton.classList.add('articleButton');
 
     title.innerHTML = "Nitro Coffee";
     description.innerHTML = "Coffee Shop & Co-Working Space";
 
-    listItem0.innerHTML = "HOME";
-    listItem1.innerHTML = "MENU";
-    listItem2.innerHTML = "ABOUT";
-    listItem3.innerHTML = "LOCATION";
+    homeTab.innerHTML = "HOME";
+    menuTab.innerHTML = "MENU";
+    aboutTab.innerHTML = "ABOUT";
+    locationTab.innerHTML = "LOCATION";
 
     mainText.innerHTML = "You don't have to stay home <br> to feel like home.";
     subText1.innerHTML = "Visit our unique space and let your creativity flow. You bring the ideas, we provide the energy.";
-    subText2.innerHTML = "A unique space in the city, where you can taste the best-in-class coffee straight from El Salvador, work from our shop with dedicated spaces or come hang out with friends and family.";
+    subText2.innerHTML = "Quote";
+    menuButton.innerHTML = "Menu";
+    infoButton.innerHTML = "Learn More";
     
     header.append(title,description,nav);
 
     nav.appendChild(list);
-    list.append(listItem0 ,listItem1, listItem2, listItem3);
+    list.append(homeTab ,menuTab, aboutTab, locationTab);
 
     main.appendChild(article);
     
-    article.append(mainText, subText1,subText2);
+    article.append(mainText, subText1,buttonBox,subText2);
+    buttonBox.append(menuButton,infoButton)
 
     content.append(header, main, footer);
+
 }
