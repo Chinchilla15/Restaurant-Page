82d8756f2b07c034af8435197ab964d52bdf6b65
Alessandro Chinchilla <ale030401@gmail.com>
2023-12-12 10:44:40 -0600

    Add menu itmes and initial styles

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,3 +1,29 @@
 export default function renderMenuTab(){
     
+    const mainContent = document.querySelector('main');
+
+    const menuDiv = document.createElement('div');
+    const hotDrink = document.createElement('p');
+    const coldDrink = document.createElement('p');
+    
+    let text = ["item0", "item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10", "item11"];
+    for(let i = 0; i < text.length; i += 1) {
+    const menuItem = document.createElement("div");
+    menuItem.classList.add('menuItem')
+    menuItem.innerHTML = text[i];
+    menuDiv.appendChild(menuItem)
+}   
+
+
+    menuDiv.classList.add('menuDiv');
+    hotDrink.classList.add('hotDrink');
+    coldDrink.classList.add('coldDrink');
+
+    hotDrink.innerHTML = 'Hot Drinks';
+    coldDrink.innerHTML = 'Cold Drinks'
+
+    menuDiv.append(hotDrink,coldDrink)
+
+    mainContent.appendChild(menuDiv);
+
 }
