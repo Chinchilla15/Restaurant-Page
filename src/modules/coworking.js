85242e9c7b6ef70fa01b85cd3465c50077e55484
Alessandro Chinchilla <ale030401@gmail.com>
2023-12-13 12:04:26 -0600

    Add: main text and animation

--- src/modules/coworking.js
+++ src/modules/coworking.js
@@ -1,10 +1,15 @@
 export default function renderCoworkingTab(){
     const mainContent = document.querySelector('main');
     
+    const mainDiv = document.createElement('div');
     const mainText = document.createElement('h3');
+
+    mainDiv.classList.add('mainDiv');
+    mainText.classList.add('comingSoon')
 
     mainText.innerHTML = "Coming Soon!";
 
 
-    mainContent.appendChild(mainText);
-};
+    mainContent.appendChild(mainDiv);
+    mainDiv.append(mainText);
+}
