4652304058c404e502fed0ebbf6eb9ff67a2f4a5
Alessandro Chinchilla <ale030401@gmail.com>
2023-12-13 10:46:50 -0600

    Adding coworking module and style files

---
+++ src/modules/coworking.js
@@ -0,0 +1,10 @@
+export default function renderCoworkingTab(){
+    const mainContent = document.querySelector('main');
+    
+    const mainText = document.createElement('h3');
+
+    mainText.innerHTML = "Coming Soon!";
+
+
+    mainContent.appendChild(mainText);
+};
