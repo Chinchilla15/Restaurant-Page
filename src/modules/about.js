018f633b0b9ccda9fad1073934669be8b2f9405b
Alessandro Chinchilla <ale030401@gmail.com>
2023-12-13 10:47:35 -0600

    Add initial content to about tab + styles

--- src/modules/about.js
+++ src/modules/about.js
@@ -1,3 +1,14 @@
 export default function renderAboutTab(){
+    const mainContent = document.querySelector('main');
     
-}
+    const mainDiv = document.createElement('div');
+    const aboutHeader = document.createElement('h3');
+
+    mainDiv.classList.add('mainDiv');
+
+    aboutHeader.innerHTML = "For the coffe lovers";
+
+
+    mainContent.appendChild(mainDiv);
+    mainDiv.append(aboutHeader);
+};
